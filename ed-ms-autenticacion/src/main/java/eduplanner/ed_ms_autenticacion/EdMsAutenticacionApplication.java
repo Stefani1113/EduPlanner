fd8f8f8423da17d8d4b10f081be0f3c5207d3ce6
Stefani1113 <stefaniapuerta604@gmail.com>
2026-05-23 17:18:31 -0500

    fix: convertir submodulo en carpeta normal

---
+++ ed-ms-autenticacion/src/main/java/eduplanner/ed_ms_autenticacion/EdMsAutenticacionApplication.java
@@ -0,0 +1,13 @@
+package eduplanner.ed_ms_autenticacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdMsAutenticacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdMsAutenticacionApplication.class, args);
+	}
+
+}
---
+++ ed-ms-autenticacion/src/test/java/eduplanner/ed_ms_autenticacion/EdMsAutenticacionApplicationTests.java
@@ -0,0 +1,13 @@
+package eduplanner.ed_ms_autenticacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdMsAutenticacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
