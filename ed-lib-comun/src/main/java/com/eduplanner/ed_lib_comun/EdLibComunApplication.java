81d6b9bd10382fdc28a78a7d0db2859d611babf9
Stefani1113 <stefaniapuerta604@gmail.com>
2026-05-28 06:48:12 -0500

    feat: Implementación de libreria común

---
+++ ed-lib-comun/src/main/java/com/eduplanner/ed_lib_comun/EdLibComunApplication.java
@@ -0,0 +1,13 @@
+package com.eduplanner.ed_lib_comun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdLibComunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdLibComunApplication.class, args);
+	}
+
+}
---
+++ ed-lib-comun/src/test/java/com/eduplanner/ed_lib_comun/EdLibComunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduplanner.ed_lib_comun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdLibComunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
