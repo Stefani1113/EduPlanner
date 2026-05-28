d9750b03b61020143d41a958f90c98887fc15502
Stefani1113 <stefaniapuerta604@gmail.com>
2026-05-28 07:22:49 -0500

    feat: Implemetación de config

---
+++ ed-ms-autenticacion/src/main/java/com/eduplanner/ed_ms_autenticacion/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.eduplanner.ed_ms_autenticacion.config;
+import com.eduplanner.ed_ms_autenticacion.security.RoleInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+
+    private final RoleInterceptor roleInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(roleInterceptor);
+    }
+}
