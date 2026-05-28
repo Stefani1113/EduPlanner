a86d2a1936380c3f0502a26a751a8cc5351ff9a0
kariangelsilva <kariangelsilva14@gmail.com>
2026-05-28 09:02:47 -0500

    feat: agregar el UsuarioRepository

---
+++ ed-ms-autenticacion/src/main/java/com/eduplanner/ed_ms_autenticacion/repository/UsuarioRepository.java
@@ -0,0 +1,16 @@
+package eduplanner.ed_ms_autenticacion.repository;
+
+import eduplanner.ed_ms_autenticacion.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
+    boolean existsByDocument(String document);
+    List<User> findByIdRole(Integer idRole);
+}
