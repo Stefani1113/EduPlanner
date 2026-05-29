685c979d7fef0b30460030e4ff5ac8acee8bfd99
Stefani1113 <stefaniapuerta604@gmail.com>
2026-05-28 19:58:06 -0500

    feat: Se agrega microservicio de administracion

---
+++ ed-ms-administracion/src/test/java/com/eduplanner/ed_ms_administracion/EdMsAdministracionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduplanner.ed_ms_administracion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdMsAdministracionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
