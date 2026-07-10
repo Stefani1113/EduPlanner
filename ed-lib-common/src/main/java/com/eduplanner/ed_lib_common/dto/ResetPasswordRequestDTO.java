d9e0b3845bb595f26a5e55fba1ff8ebe352c17e2
Stefani1113 <stefaniapuerta604@gmail.com>
2026-07-09 20:01:58 -0500

    feat: Se agrego validación de contraseña

--- ed-lib-common/src/main/java/com/eduplanner/ed_lib_common/dto/ResetPasswordRequestDTO.java
+++ ed-lib-common/src/main/java/com/eduplanner/ed_lib_common/dto/ResetPasswordRequestDTO.java
@@ -1,5 +1,7 @@
 package com.eduplanner.ed_lib_common.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
@@ -8,11 +10,24 @@ public class ResetPasswordRequestDTO {
     /**
      * Token generado para la restauración de contraseña
      */
+    @NotBlank(message = "El token es obligatorio")
     private String token;
 
     /**
      * Nueva contraseña del usuario
      */
+    @NotBlank(message = "La nueva contraseña es obligatoria")
+
+    /**
+     * Expresión regular para validación de contraseña
+     */
+    @Pattern(
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&#.\\-_])[A-Za-z\\d@$!%*?&#.\\-_]{8,}$",
+        message = "La contraseña debe tener al menos 8 caracteres, una mayúscula, una minúscula, un número y un carácter especial"
+    )
+
     private String newPassword;
+
+
 
 }
