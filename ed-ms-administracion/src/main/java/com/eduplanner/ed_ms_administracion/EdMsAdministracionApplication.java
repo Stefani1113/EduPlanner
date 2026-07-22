272297486642f76a94d9197ed1d9e73e21431272
Stefani1113 <stefaniapuerta604@gmail.com>
2026-07-22 18:30:41 -0500

    feat: Se añadio ms administración y se configuro pom y .yaml

---
+++ ed-ms-administracion/src/main/java/com/eduplanner/ed_ms_administracion/EdMsAdministracionApplication.java
@@ -0,0 +1,13 @@
+package com.eduplanner.ed_ms_administracion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdMsAdministracionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdMsAdministracionApplication.class, args);
+	}
+
+}
