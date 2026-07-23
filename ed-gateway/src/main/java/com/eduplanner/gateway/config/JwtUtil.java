8991fe169d00ec540db911e83805a74fa3fd7bfb
kariangelsilva <kariangelsilva14@gmail.com>
2026-07-23 18:40:38 -0500

    feat: Agregar validacion del token de JwtUtil en config

--- ed-gateway/src/main/java/com/eduplanner/gateway/config/JwtUtil.java
+++ ed-gateway/src/main/java/com/eduplanner/gateway/config/JwtUtil.java
@@ -1,7 +1,43 @@
 package com.eduplanner.gateway.config;
 
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.function.Function;
+import java.util.logging.Logger;
+
+
+@Component
 public class JwtUtil {
 
+    private static final Logger log = Logger.getLogger(JwtUtil.class.getName());
 
+    @Value("${security.jwt.secret-key}")
+    private String secretKey;
+
+    private SecretKey getSignKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
+
+    
+    public boolean isTokenValid(String token) {
+        try {
+            Jwts.parser().verifyWith(getSignKey()).build().parseSignedClaims(token);
+            return true;
+        } catch (JwtException e) {
+            log.warning("Token inválido o expirado: " + e.getMessage());
+            return false;
+        } catch (Exception e) {
+            log.severe("Error validando token: " + e.getMessage());
+            return false;
+        }
+    }
 }
