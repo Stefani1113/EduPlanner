258be3246663b123b23bc09d3da28aa96a761814
kariangelsilva <kariangelsilva14@gmail.com>
2026-07-23 07:12:44 -0500

    feat: agregar los archivos de config

--- ed-gateway/src/main/java/com/eduplanner/gateway/EdGatewayApplication.java
+++ ed-gateway/src/main/java/com/eduplanner/gateway/EdGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.eduplaner.gateway;
+package com.eduplanner.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
