c817a47924ca56c7b331c0be2a7115942e757e2b
kariangelsilva <kariangelsilva14@gmail.com>
2026-07-23 21:18:25 -0500

    feat: Agregar  Filtro global JWT del Gateway

--- ed-gateway/src/main/java/com/eduplanner/gateway/filter/JwtAuth.java
+++ ed-gateway/src/main/java/com/eduplanner/gateway/filter/JwtAuth.java
@@ -1,5 +1,32 @@
 package com.eduplanner.gateway.filter;
 
-public class JwtAuth{
-    
+import com.eduplanner.gateway.config.GatewayConfig;
+import com.eduplanner.gateway.config.JwtUtil;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+
+@Component
+public class JwtAuthFilter implements GlobalFilter, Ordered {
+
+    private static final Logger log = Logger.getLogger(JwtAuthFilter.class.getName());
+
+    private final JwtUtil jwtUtil;
+    private final GatewayConfig gatewayConfig;
+
+    public JwtAuthFilter(JwtUtil jwtUtil, GatewayConfig gatewayConfig) {
+        this.jwtUtil = jwtUtil;
+        this.gatewayConfig = gatewayConfig;
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        String path = request.getURI().getPath();
+    }
 }
