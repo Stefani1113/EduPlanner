299fb548b9f3975281eadc9d04bd5f62f54ec012
Stefani1113 <stefaniapuerta604@gmail.com>
2026-07-24 10:03:06 -0500

    feat: Se añadieron DTOs para token de activación de cuenta

---
+++ ed-lib-common/src/main/java/com/eduplanner/ed_lib_common/dto/ActivationTokenResponseDTO.java
@@ -0,0 +1,13 @@
+package com.eduplanner.ed_lib_common.dto;
+
+import lombok.Data;
+
+/**
+ * DTo de respuesta con token de activación
+ * ActivationTokenResponseDTO
+ */
+@Data
+public class ActivationTokenResponseDTO {
+    
+    private String token;
+}
