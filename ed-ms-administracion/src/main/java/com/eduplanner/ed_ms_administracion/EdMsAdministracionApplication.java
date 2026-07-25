57ae34e98bfe12da926cb8a4a89b37e64efa34cf
Stefani1113 <stefaniapuerta604@gmail.com>
2026-07-25 18:23:51 -0500

    feat: Se añade Entity scan en clase principal

--- ed-ms-administracion/src/main/java/com/eduplanner/ed_ms_administracion/EdMsAdministracionApplication.java
+++ ed-ms-administracion/src/main/java/com/eduplanner/ed_ms_administracion/EdMsAdministracionApplication.java
@@ -2,8 +2,11 @@ package com.eduplanner.ed_ms_administracion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.eduplanner.ed_lib_common.entity")
+
 public class EdMsAdministracionApplication {
 
 	public static void main(String[] args) {
