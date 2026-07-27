408cba4bea4fe9796c42b611240655b80ca2ddfc
Stefani1113 <stefaniapuerta604@gmail.com>
2026-07-27 08:15:13 -0500

    feat: Se añadio metodo estático a UserResponseDTO

--- ed-lib-common/src/main/java/com/eduplanner/ed_lib_common/dto/UserResponseDTO.java
+++ ed-lib-common/src/main/java/com/eduplanner/ed_lib_common/dto/UserResponseDTO.java
@@ -3,11 +3,13 @@ package com.eduplanner.ed_lib_common.dto;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import com.eduplanner.ed_lib_common.entity.User;
+
 import lombok.Data;
 
 @Data
 public class UserResponseDTO {
-    
+
     private Integer idUser;
 
     private String email;
@@ -63,4 +65,40 @@ public class UserResponseDTO {
     private Integer idRole;
 
     private Integer idInstitution;
+
+    /**
+     * Metodo estático 
+     */
+
+    public static UserResponseDTO fromEntity(User user) {
+        UserResponseDTO dto = new UserResponseDTO();
+        dto.setIdUser(user.getIdUser());
+        dto.setEmail(user.getEmail());
+        dto.setName(user.getName());
+        dto.setSurnames(user.getSurnames());
+        dto.setDocumentType(user.getDocumentType());
+        dto.setDocument(user.getDocument());
+        dto.setDocumentIssuePlace(user.getDocumentIssuePlace());
+        dto.setBirthdate(user.getBirthdate());
+        dto.setPhoneNumber(user.getPhoneNumber());
+        dto.setStatus(user.getStatus());
+        dto.setPhotoUrl(user.getPhotoUrl());
+        dto.setProfessionalDegrees(user.getProfessionalDegrees());
+        dto.setQualificationsDesc(user.getQualificationsDesc());
+        dto.setGender(user.getGender());
+        dto.setAddress(user.getAddress());
+        dto.setBloodType(user.getBloodType());
+        dto.setDisabilities(user.getDisabilities());
+        dto.setStratum(user.getStratum());
+        dto.setPopulationType(user.getPopulationType());
+        dto.setHealthRegime(user.getHealthRegime());
+        dto.setEps(user.getEps());
+        dto.setPosition(user.getPosition());
+        dto.setCreationDate(user.getCreationDate());
+        dto.setUpdateDate(user.getUpdateDate());
+        dto.setLastAccess(user.getLastAccess());
+        dto.setRoleName(user.getRole().getName());
+        dto.setIdRole(user.getRole().getIdRole());
+        return dto;
+    }
 }
