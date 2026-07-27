692c942827f6379e7b64702a7f3f2f46c8364abb
Stefani1113 <stefaniapuerta604@gmail.com>
2026-07-27 08:20:19 -0500

    feat: Se añadio UserRepository

--- ed-ms-administracion/src/main/java/com/eduplanner/ed_ms_administracion/repository/UserRepository.java
+++ ed-ms-administracion/src/main/java/com/eduplanner/ed_ms_administracion/repository/UserRepository.java
@@ -1,9 +1,13 @@
 package com.eduplanner.ed_ms_administracion.repository;
 
 import com.eduplanner.ed_lib_common.entity.User;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     boolean existsByEmail(String email);
     boolean existsByDocument(String document);
+    List<User> findByRoleIdRole(Integer idRole);
 }
