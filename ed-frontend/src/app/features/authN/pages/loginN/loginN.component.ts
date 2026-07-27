37709f1ec0b0ac81d72531fa538910f79602a83a
Pedrito190uwu <mariagaribello190@gmail.com>
2026-07-27 17:23:03 -0500

    fix:crear login el aterrior era nueva contraseña

---
+++ ed-frontend/src/app/features/authN/pages/loginN/loginN.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login-new_contraseña',
+  standalone: false,
+  templateUrl: './login-new_contraseña.component.html',
+  styleUrl: './login-new_contraseña.component.scss'
+})
+export class LoginComponent {
+
+}
