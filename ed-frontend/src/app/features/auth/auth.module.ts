524c1c9afc53f1033b89fd289c71be1ba190c72b
Pedrito190uwu <mariagaribello190@gmail.com>
2026-07-28 16:57:07 -0500

    fix: orden de carpetas

--- ed-frontend/src/app/features/auth/auth.module.ts
+++ ed-frontend/src/app/features/auth/auth.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    AuthRoutingModule
-  ]
+    AuthRoutingModule,
+    LoginPageComponent
+  ],
 })
 export class AuthModule { }
