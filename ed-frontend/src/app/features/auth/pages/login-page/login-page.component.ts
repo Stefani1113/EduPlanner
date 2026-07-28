e58c3e3c07c17ffffaa3f4c6853e366b53db8d1a
Pedrito190uwu <mariagaribello190@gmail.com>
2026-07-28 18:40:07 -0500

    feat:html echo de inicio sesion login

--- ed-frontend/src/app/features/auth/pages/login-page/login-page.component.ts
+++ ed-frontend/src/app/features/auth/pages/login-page/login-page.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login-page',
-  imports: [],
   templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss'
+  styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent {
-    
+
 }
