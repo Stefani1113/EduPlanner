be20dbc5d82aafaa5ad750fe9f391a9035646c94
Pedrito190uwu <mariagaribello190@gmail.com>
2026-07-28 23:10:06 -0500

    feat:avance conectando el frotend

--- ed-frontend/src/app/features/auth/pages/login-page/login-page.component.ts
+++ ed-frontend/src/app/features/auth/pages/login-page/login-page.component.ts
@@ -1,17 +1,48 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login-page',
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent {
 
-  constructor(private router: Router) {}
+  email: string = '';
+  password: string = '';
+
+  constructor(
+    private router: Router,
+    private authService: AuthService
+  ) {}
+
+  login(): void {
+
+    this.authService.login(this.email, this.password).subscribe({
+
+      next: (response: any) => {
+
+        localStorage.setItem('token', response.data.token);
+        localStorage.setItem('usuario', JSON.stringify(response.data));
+
+        this.router.navigate(['/dashboard']); // Cambia la ruta si tu aplicación usa otra
+
+      },
+
+      error: (error) => {
+        console.error(error);
+        alert('Correo o contraseña incorrectos.');
+      }
+
+    });
+
+  }
 
   irForgotPassword() {
-    console.log('Se hizo clic');
     this.router.navigate(['/auth/forgot-password']);
   }
 }
