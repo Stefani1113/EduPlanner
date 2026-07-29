09fe3807b6709788f846858c2d982be6c1d748be
Pedrito190uwu <mariagaribello190@gmail.com>
2026-07-28 22:01:00 -0500

    fix:acomodar para comenzar a conectar fotend con el back

--- ed-frontend/src/app/features/auth/services/auth.service.ts
+++ ed-frontend/src/app/features/auth/services/auth.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:8080/auth';
+
+  constructor(private http: HttpClient) {}
+
+  login(data: LoginRequest): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, data);
+  }
 }
