163e88bc044e4a67b44db5dfffa384419a01b2c1
Pedrito190uwu <mariagaribello190@gmail.com>
2026-07-30 18:02:59 -0500

    fix:correciones de vista de diseño y la  informacion mostrada

--- ed-frontend/src/app/features/auth/services/auth.service.ts
+++ ed-frontend/src/app/features/auth/services/auth.service.ts
@@ -1,43 +1,43 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+
 import { LoginRequest } from '../models/login-request';
-import { LoginResponse } from '../models/login-response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  models(data: LoginRequest) {
-  return this.http.post<any>(
-    'http://localhost:8081/eduplanner/auth/login',
-    data
-  );
-}
-
   private api = 'http://localhost:8081/eduplanner/auth';
 
   constructor(private http: HttpClient) {}
 
+  models(data: LoginRequest): Observable<any> {
+    return this.http.post(
+      'http://localhost:8081/eduplanner/auth/login',
+      data
+    );
+  }
+
   login(email: string, password: string): Observable<any> {
-    return this.http.post<any>(`${this.api}/login`, {
+    return this.http.post(`${this.api}/login`, {
       email,
       password
     });
   }
 
- forgotPassword(email: string): Observable<any> {
-  return this.http.post(`${this.api}/forgot-password`, {
-    email
-  });
-}
+  forgotPassword(email: string): Observable<any> {
+    return this.http.post(`${this.api}/forgot-password`, {
+      email
+    });
+  }
 
-resetPassword(token: string, password: string): Observable<any> {
-  return this.http.put(`${this.api}/reset-password`, {
-    token,
-    password
-  });
-}
+  resetPassword(token: string, newPassword: string): Observable<any> {
+    return this.http.put(`${this.api}/reset-password`, {
+      token,
+      newPassword
+    });
+  }
 
 }
