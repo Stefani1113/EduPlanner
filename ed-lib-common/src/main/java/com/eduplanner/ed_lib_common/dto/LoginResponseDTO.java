410eb7145f5c0d080df2c0840a8e1aa9065ab0a9
Stefani1113 <stefaniapuerta604@gmail.com>
2026-07-30 16:37:25 -0500

    fix: Corrección en DTO de respuesta de login

--- ed-lib-common/src/main/java/com/eduplanner/ed_lib_common/dto/LoginResponseDTO.java
+++ ed-lib-common/src/main/java/com/eduplanner/ed_lib_common/dto/LoginResponseDTO.java
@@ -4,11 +4,11 @@ import lombok.Data;
 
 @Data
 public class LoginResponseDTO {
+
     private String token;
+
     private Integer idUser;
-    private String name;
-    private String lastName;
-    private String email;
+
     private String role;
 }
 
