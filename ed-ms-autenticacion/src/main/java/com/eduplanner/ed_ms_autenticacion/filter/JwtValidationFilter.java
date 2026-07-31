967fc4bd4f0bb4286bcde16f396e9ebb3ef2f4c9
Stefani1113 <stefaniapuerta604@gmail.com>
2026-07-30 19:40:13 -0500

    feat: Cambio de nombre de IdRole a Role en el JWT

--- ed-ms-autenticacion/src/main/java/com/eduplanner/ed_ms_autenticacion/filter/JwtValidationFilter.java
+++ ed-ms-autenticacion/src/main/java/com/eduplanner/ed_ms_autenticacion/filter/JwtValidationFilter.java
@@ -49,7 +49,7 @@ public class JwtValidationFilter extends OncePerRequestFilter {
         try {
             if (jwtService.isTokenValid(token)) {
                 request.setAttribute("idUser",  jwtService.extractIdUser(token));
-                request.setAttribute("idRole",      jwtService.extractIdRole(token));
+                request.setAttribute("role",      jwtService.extractIdRole(token));
                 filterChain.doFilter(request, response);
             } else {
                 sendError(response, HttpServletResponse.SC_UNAUTHORIZED,
