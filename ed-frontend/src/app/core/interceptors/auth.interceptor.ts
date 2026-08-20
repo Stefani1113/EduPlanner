c30465e4f2650b3bf4377a5c0247ed052789c8e9
Pedrito190uwu <mariagaribello190@gmail.com>
2026-08-20 17:40:50 -0500

    fix:renovacion de token JWT

--- ed-frontend/src/app/core/interceptors/auth.interceptor.ts
+++ ed-frontend/src/app/core/interceptors/auth.interceptor.ts
@@ -1,11 +1,16 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { AuthService } from '../../features/auth/services/auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+  const authService = inject(AuthService);
   const token = localStorage.getItem('token');
 
   if (!token) {
     return next(req);
   }
+
+  authService.registrarActividadDesdeInterceptor();
 
   const reqConToken = req.clone({
     setHeaders: {
