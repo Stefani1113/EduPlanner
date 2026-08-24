c9e35e6d15530d3f8f55093cd974278edf47e873
Stefani1113 <stefaniapuerta604@gmail.com>
2026-08-23 19:06:22 -0500

    feat: Se añadio Repository de Period

---
+++ ed-ms-gestion-academica/src/main/java/com/EduPlanner/ed_ms_gestion_academica/repository/AcademicPeriodRepository.java
@@ -0,0 +1,9 @@
+package com.EduPlanner.ed_ms_gestion_academica.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.eduplanner.ed_lib_common.entity.AcademicPeriod;
+
+public interface AcademicPeriodRepository extends JpaRepository<AcademicPeriod, Integer> {
+    boolean existsByName(String name);
+}
