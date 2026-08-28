1c82338884249e7e773e80e717e69a163420d2c8
kariangelsilva <kariangelsilva14@gmail.com>
2026-08-27 22:09:00 -0500

    feat: Agregar metodo en AttendanceRepository

--- ed-ms-gestion-academica/src/main/java/com/EduPlanner/ed_ms_gestion_academica/repository/AttendanceRepository.java
+++ ed-ms-gestion-academica/src/main/java/com/EduPlanner/ed_ms_gestion_academica/repository/AttendanceRepository.java
@@ -4,6 +4,7 @@ import com.eduplanner.ed_lib_common.entity.Attendance;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 public interface AttendanceRepository extends JpaRepository<Attendance, Integer> {
@@ -11,4 +12,12 @@ public interface AttendanceRepository extends JpaRepository<Attendance, Integer>
     boolean existsByIdScheduleAndIdStudentAndAttendanceDate(Integer idSchedule, Integer idStudent, LocalDate attendanceDate);
 
     Optional<Attendance> findByIdScheduleAndIdStudentAndAttendanceDate(Integer idSchedule, Integer idStudent, LocalDate attendanceDate);
+
+    /** Historial de un estudiante en un periodo */
+    List<Attendance> findByIdStudentAndAttendanceDateBetweenOrderByAttendanceDateAsc(
+            Integer idStudent, LocalDate startDate, LocalDate endDate);
+
+    /** Historial de un curso/grupo en un periodo */
+    List<Attendance> findByIdCourseAndAttendanceDateBetweenOrderByAttendanceDateAsc(
+            Integer idCourse, LocalDate startDate, LocalDate endDate);
 }
