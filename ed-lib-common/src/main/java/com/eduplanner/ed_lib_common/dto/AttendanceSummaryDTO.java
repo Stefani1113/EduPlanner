6a75c2285fe0a329926679f97020f7df39349469
kariangelsilva <kariangelsilva14@gmail.com>
2026-08-28 20:10:00 -0500

    feat: Agregar el DTO de resumen personal

---
+++ ed-lib-common/src/main/java/com/eduplanner/ed_lib_common/dto/AttendanceSummaryDTO.java
@@ -0,0 +1,24 @@
+package com.eduplanner.ed_lib_common.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+/** HU 4.5 - Resumen personal de asistencia (estadísticas de un estudiante en un periodo) */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AttendanceSummaryDTO {
+    private Integer idStudent;
+    private LocalDate startDate;
+    private LocalDate endDate;
+    private long totalRecords;
+    private long presentCount;
+    private long lateCount;
+    private long earlyDepartureCount;
+    private long justifiedAbsenceCount;
+    private long unjustifiedAbsenceCount;
+    private double attendancePercentage;
+}
