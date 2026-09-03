bf75607547e011e0b91ae1cd623a6c462564c12f
kariangelsilva <kariangelsilva14@gmail.com>
2026-09-03 12:51:30 -0500

    feat: Agregar el repository

---
+++ ed_ms_notas/src/main/java/EduPlanner/ed_ms_notas/repository/EvaluationTypeRepository.java
@@ -0,0 +1,7 @@
+package eduPlanner.ed_ms_notas.repository;
+
+import com.eduplanner.ed_lib_common.entity.EvaluationType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EvaluationTypeRepository extends JpaRepository<EvaluationType, Integer> {
+}
