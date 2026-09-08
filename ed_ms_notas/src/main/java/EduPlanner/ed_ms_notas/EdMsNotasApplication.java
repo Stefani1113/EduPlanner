c8fbb57f4fdd2900d730d868dc7292887dc6c5db
kariangelsilva <kariangelsilva14@gmail.com>
2026-09-08 08:51:32 -0500

    feat: Agregar cambio

--- ed_ms_notas/src/main/java/EduPlanner/ed_ms_notas/EdMsNotasApplication.java
+++ ed_ms_notas/src/main/java/EduPlanner/ed_ms_notas/EdMsNotasApplication.java
@@ -1,13 +1,17 @@
-package EduPlanner.ed_ms_notas;
+package eduPlanner.ed_ms_notas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.persistence.autoconfigure.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {
+    "eduPlanner.ed_ms_notas",
+    "com.eduplanner.ed_lib_common.entity"
+})
 public class EdMsNotasApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EdMsNotasApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EdMsNotasApplication.class, args);
+    }
 }
