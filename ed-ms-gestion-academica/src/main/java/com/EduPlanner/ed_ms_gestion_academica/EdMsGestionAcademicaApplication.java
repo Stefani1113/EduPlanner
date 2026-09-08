8d6e7bda7d3af21ada4a523c818bf626eb136050
Stefani1113 <stefaniapuerta604@gmail.com>
2026-09-08 18:53:30 -0500

    feat: Se añadio consul y feign, con ruta publica de estado

--- ed-ms-gestion-academica/src/main/java/com/EduPlanner/ed_ms_gestion_academica/EdMsGestionAcademicaApplication.java
+++ ed-ms-gestion-academica/src/main/java/com/EduPlanner/ed_ms_gestion_academica/EdMsGestionAcademicaApplication.java
@@ -3,7 +3,9 @@ package com.EduPlanner.ed_ms_gestion_academica;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.persistence.autoconfigure.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 @EntityScan(basePackages = "com.eduplanner.ed_lib_common.entity")
 public class EdMsGestionAcademicaApplication {
