cced510f1fad1f1a8733fd5e7108d82a9559c987
Stefani1113 <stefaniapuerta604@gmail.com>
2026-09-08 17:14:48 -0500

    feat: Se añadieron dependencias al pom

--- ed-ms-administracion/src/main/java/com/eduplanner/ed_ms_administracion/client/GestionAcademicaServiceClient.java
+++ ed-ms-administracion/src/main/java/com/eduplanner/ed_ms_administracion/client/GestionAcademicaServiceClient.java
@@ -1,61 +1,16 @@
 package com.eduplanner.ed_ms_administracion.client;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
-@Component
-@RequiredArgsConstructor
-@Log4j2
-public class GestionAcademicaServiceClient {
+@FeignClient(name = "ed-ms-gestion-academica")
+public interface GestionAcademicaServiceClient {
 
-    private final RestTemplate restTemplate;
-
-    @Value("${services.gestion-academica.base-url}")
-    private String gestionAcademicaBaseUrl;
-
-    public void adjustCourseStudentCount(Integer idCourse, int delta) {
-
-        if (idCourse == null) {
-            log.warn("No se puede actualizar contador: idCourse es null");
-            return;
-        }
-
-        String url = gestionAcademicaBaseUrl
-                + "/eduplanner/internal/courses/"
-                + idCourse
-                + "/student-count?delta="
-                + delta;
-
-        log.info(">>> ENVIANDO PUT INTERNO: {}", url);
-
-        try {
-
-            ResponseEntity<Void> response = restTemplate.exchange(
-                    url,
-                    HttpMethod.PUT,
-                    null,
-                    Void.class
-            );
-
-            log.info(
-                    ">>> RESPUESTA GESTION ACADEMICA: {}",
-                    response.getStatusCode()
-            );
-
-        } catch (Exception e) {
-
-            log.error(
-                    ">>> ERROR ACTUALIZANDO CONTADOR DEL CURSO {}",
-                    idCourse,
-                    e
-            );
-
-            throw e;
-        }
-    }
+    @PutMapping("/eduplanner/internal/courses/{idCourse}/student-count")
+    void adjustCourseStudentCount(
+            @PathVariable("idCourse") Integer idCourse,
+            @RequestParam("delta") int delta
+    );
 }
