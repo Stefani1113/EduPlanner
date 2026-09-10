4e2112b6b86dd734a3e4bf44dbfa691789b3965a
Stefani1113 <stefaniapuerta604@gmail.com>
2026-09-10 06:21:12 -0500

    feat: Se agrego nueva ruta http para comunicación entre microservicios

--- ed-ms-gestion-academica/src/main/java/com/EduPlanner/ed_ms_gestion_academica/client/AdministracionServiceClient.java
+++ ed-ms-gestion-academica/src/main/java/com/EduPlanner/ed_ms_gestion_academica/client/AdministracionServiceClient.java
@@ -1,15 +1,29 @@
 package com.EduPlanner.ed_ms_gestion_academica.client;
 
+import java.util.List;
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
+import com.eduplanner.ed_lib_common.dto.UserResponseDTO;
 
 @FeignClient(name = "ed-ms-administracion")
 public interface AdministracionServiceClient {
 
     /**
      * Devuelve el nombre del rol del usuario, o null si no existe.
+     * @param idUser
+     * @return
      */
     @GetMapping("/eduplanner/internal/users/{idUser}/role")
     String getUserRole(@PathVariable("idUser") Integer idUser);
+
+    /**
+     *Devuelve usuarios de dicho curso
+     * @param idCourse
+     * @return
+     */
+    @GetMapping("/eduplanner/internal/user/courses/{idCourse}")
+    List<UserResponseDTO> getUsersByCourse(@PathVariable("idCourse") Integer idCourse);
 }
