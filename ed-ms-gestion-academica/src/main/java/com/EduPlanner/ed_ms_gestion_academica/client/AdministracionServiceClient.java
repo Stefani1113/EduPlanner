b1cda5accc1467ab1c9f49d592f451aa5772935d
kariangelsilva <kariangelsilva14@gmail.com>
2026-09-13 16:55:21 -0500

    fix: Elimar algunos problemas

--- ed-ms-gestion-academica/src/main/java/com/EduPlanner/ed_ms_gestion_academica/client/AdministracionServiceClient.java
+++ ed-ms-gestion-academica/src/main/java/com/EduPlanner/ed_ms_gestion_academica/client/AdministracionServiceClient.java
@@ -1,10 +1,11 @@
 package com.EduPlanner.ed_ms_gestion_academica.client;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+
+import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
@@ -15,19 +16,6 @@ public class AdministracionServiceClient {
     @Value("${services.administracion.base-url}")
     private String administracionBaseUrl;
 
-    /**
-     * Devuelve el nombre del rol del usuario, o null si no existe.
-     */
-    public String getUserRole(Integer idUser) {
-        try {
-            return restTemplate.getForObject(
-                    administracionBaseUrl + "/internal/users/" + idUser + "/role",
-                    String.class
-            );
-        } catch (HttpClientErrorException.NotFound e) {
-            return null;
-        }
-    }
 
     /**
      * Devuelve el nombre completo del usuario (estudiante, docente, etc.), o null si no existe.
