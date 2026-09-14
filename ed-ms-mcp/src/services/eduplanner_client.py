df7bfd300468020513ecd7ad7e4b7fb5036c4ffa
Stefani1113 <stefaniapuerta604@gmail.com>
2026-09-14 09:42:39 -0500

    feat: Se añadio ruta para ingresar datos

--- ed-ms-mcp/src/services/eduplanner_client.py
+++ ed-ms-mcp/src/services/eduplanner_client.py
@@ -5,6 +5,7 @@ import requests
 class EduPlannerClient : 
     """Http Client de Eduplanner"""
 
+# Llamada get para traer datos 
     def __init__(self, settings, jwt_manager):
         self.settings = settings
         self.jwt_manager = jwt_manager
@@ -25,6 +26,31 @@ class EduPlannerClient :
 
         response = requests.get(
             url, headers= headers,
+        )
+
+        response.raise_for_status()
+
+# Llamada post para ingresar datos 
+    def post(self, endpoints: str, data: dict) -> Any :
+        """Realiza una solicitud POST autenticada a EduPlanner"""
+
+        token = self.jwt_manager.get_valid_token()
+
+        headers = {
+            "Authorization" : f"Bearer {token}",
+            "Content-Type" : "application/json"
+        }
+
+        url = (
+            f"{self.settings.EDUPLANNER_BASE_URL}"
+            f"{endpoints}"
+        )
+
+# Manda JSON que genera el algoritmo a gestión académica
+        response = requests.post(
+            url, 
+            headers = headers,
+            json = data
         )
 
         response.raise_for_status()
