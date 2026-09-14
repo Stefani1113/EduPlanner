c28c5dd8fd1ba7f3f0075bdf95c1c8df36450071
Stefani1113 <stefaniapuerta604@gmail.com>
2026-09-14 07:17:01 -0500

    feat: Se añadieron DTOs de solicitud de horarios

---
+++ ed-lib-common/src/main/java/com/eduplanner/ed_lib_common/dto/ScheduleGenerationRequestDTO.java
@@ -0,0 +1,34 @@
+package com.eduplanner.ed_lib_common.dto;
+
+
+import com.eduplanner.ed_lib_common.entity.ScheduleType;
+
+import jakarta.persistence.GenerationType;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+
+import java.util.List;
+
+@Data
+public class ScheduleGenerationRequestDTO {
+
+    @NotNull
+    private Integer idPeriod;
+
+    @NotNull
+    private GenerationType generationType;
+
+    @NotNull
+    private Integer generatedBy;
+
+    private String observations;
+
+    private ScheduleType scheduleType = ScheduleType.REGULAR;
+
+    @NotEmpty
+    @Valid
+    private List<ScheduleItemRequestDTO> schedules;
+}
