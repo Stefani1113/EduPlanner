9a2823e1545c446e37cfc8449806902d575f9560
Stefani1113 <stefaniapuerta604@gmail.com>
2026-09-16 05:51:05 -0500

    feat: Se implemento nueva llamada para administracion

--- ed-ms-gestion-academica/src/main/java/com/EduPlanner/ed_ms_gestion_academica/client/AdministracionServiceClient.java
+++ ed-ms-gestion-academica/src/main/java/com/EduPlanner/ed_ms_gestion_academica/client/AdministracionServiceClient.java
@@ -26,7 +26,7 @@ public interface AdministracionServiceClient {
      * @param idCourse ID del curso
      * @return lista de usuarios
      */
-    @GetMapping("/eduplanner/internal/user/courses/{idCourse}")
+    @GetMapping("/eduplanner/internal/users/courses/{idCourse}")
     List<UserResponseDTO> getUsersByCourse(
             @PathVariable("idCourse") Integer idCourse
     );
