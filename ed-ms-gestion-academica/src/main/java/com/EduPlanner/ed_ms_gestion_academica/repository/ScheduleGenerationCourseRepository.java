64496b7be9095d3b7f0a6481e025cb2198815a60
Stefani1113 <stefaniapuerta604@gmail.com>
2026-09-17 20:12:53 -0500

    feat: Se agrego funcionalidad para previsualizar horario

--- ed-ms-gestion-academica/src/main/java/com/EduPlanner/ed_ms_gestion_academica/repository/ScheduleGenerationCourseRepository.java
+++ ed-ms-gestion-academica/src/main/java/com/EduPlanner/ed_ms_gestion_academica/repository/ScheduleGenerationCourseRepository.java
@@ -3,6 +3,5 @@ package com.EduPlanner.ed_ms_gestion_academica.repository;
 import com.eduplanner.ed_lib_common.entity.ScheduleGenerationCourse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ScheduleGenerationCourseRepository
-        extends JpaRepository<ScheduleGenerationCourse, Integer> {
+public interface ScheduleGenerationCourseRepository extends JpaRepository<ScheduleGenerationCourse, Integer> {
 }
--- ed-ms-gestion-academica/src/main/java/com/EduPlanner/ed_ms_gestion_academica/repository/ScheduleGenerationRepository.java
+++ ed-ms-gestion-academica/src/main/java/com/EduPlanner/ed_ms_gestion_academica/repository/ScheduleGenerationRepository.java
@@ -3,6 +3,5 @@ package com.EduPlanner.ed_ms_gestion_academica.repository;
 import com.eduplanner.ed_lib_common.entity.ScheduleGeneration;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ScheduleGenerationRepository
-        extends JpaRepository<ScheduleGeneration, Integer> {
+public interface ScheduleGenerationRepository extends JpaRepository<ScheduleGeneration, Integer> {
 }
