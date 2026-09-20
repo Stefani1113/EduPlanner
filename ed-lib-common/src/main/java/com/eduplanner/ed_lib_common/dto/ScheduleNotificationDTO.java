622d8c7a62fd7a17a0088bf8af5b9ce48c91641f
Stefani1113 <stefaniapuerta604@gmail.com>
2026-09-19 22:02:12 -0500

    fix: Correción en importaciones

--- ed-lib-common/src/main/java/com/eduplanner/ed_lib_common/dto/ScheduleNotificationDTO.java
+++ ed-lib-common/src/main/java/com/eduplanner/ed_lib_common/dto/ScheduleNotificationDTO.java
@@ -1,15 +1,16 @@
 package com.eduplanner.ed_lib_common.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
+import lombok.NoArgsConstructor;
 
-@Data 
-@RequiredArgsConstructor 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ScheduleNotificationDTO {
+
     private String type;
     private String title;
     private String message;
-    private Integer idCourse;
-    private Integer idTeacher;
+    private Integer idSchedule;
 }
-
