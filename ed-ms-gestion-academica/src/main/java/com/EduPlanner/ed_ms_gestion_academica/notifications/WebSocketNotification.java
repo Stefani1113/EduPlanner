390175b3b14b677b245290c412f4f4b13789110e
Stefani1113 <stefaniapuerta604@gmail.com>
2026-09-20 22:37:30 -0500

    fix: Correción en archivo y rutas de notificaciones

--- ed-ms-gestion-academica/src/main/java/com/EduPlanner/ed_ms_gestion_academica/notifications/WebSocketNotification.java
+++ ed-ms-gestion-academica/src/main/java/com/EduPlanner/ed_ms_gestion_academica/notifications/WebSocketNotification.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class WebSocketNotification implements Notification {
 
+
         private final SimpMessagingTemplate messagingTemplate;
 
         @Override
