316ac3f28d744e2388e88ecdedc061cc491a62b1
Pedrito190uwu <mariagaribello190@gmail.com>
2026-09-22 22:01:05 -0500

    fix:arreglar filtros de vista horarios

--- ed-frontend/src/app/features/admin/pages/horarios/horarios.component.ts
+++ ed-frontend/src/app/features/admin/pages/horarios/horarios.component.ts
@@ -351,10 +351,15 @@ export class HorariosComponent implements OnInit, OnDestroy {
         this.cargandoPerfil = false;
       }
 
+<<<<<<< Updated upstream
     });
 
     this.horariosService.obtenerDocentes().subscribe({
 
+=======
+  private cargarHorario(idCourse: number | null): void {
+    this.horariosService.obtenerMiHorario().subscribe({
+>>>>>>> Stashed changes
       next: respuesta => {
         this.docentesDisponibles =
           respuesta?.data || [];
